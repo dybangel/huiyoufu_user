77bc86c1929b2508c633a920bc0811abe28723d2
dybangel <176682769@qq.com>
2019-01-11 01:07:22 +0800

    -a

--- chat/js/socket.js
+++ chat/js/socket.js
@@ -1,50 +1,7 @@
-/*发送消息*/
-//function send(headSrc,str){
-//	var html="<div class='send'><div class='msg'><img src="+headSrc+" />"+
-//	"<p><i class='msg_input'></i>"+str+"</p></div></div>";
-//	upView(html);
-//}
-/*接受消息*/
-function show(headSrc,str){
-	var html="<div class='show'><div class='msg'><img src="+headSrc+" />"+
-	"<p><i class='msg_input'></i>"+str+"</p></div></div>";
-	upView(html);
-}
-/*更新视图*/
-function upView(html){
-	$('.message').append(html);
-	$('body').animate({scrollTop:$('.message').outerHeight()-window.innerHeight},200)
-}
-function sj(){
-	return parseInt(Math.random()*10)
-}
+var $ = jQuery.noConflict();
 $(function(){
-	$('.footer').on('keyup','input',function(){
-		if($(this).val().length>0){
-			$(this).next().css('background','#114F8E').prop('disabled',true);
-		
-		}else{
-			$(this).next().css('background','#ddd').prop('disabled',false);
-		}
-	})
-	$('.footer p').click(function(){
-		show("./images/touxiangm.png",$(this).prev().val());
-		//test();
-	})
-})
-
-/*测试数据*/
-//var arr=['我是小Q','好久没联系了！','你在想我么','怎么不和我说话','跟我聊会天吧'];
-//var imgarr=['images/touxiang.png','images/touxiangm.png']
-//test()
-//function test(){
-//	$(arr).each(function(i){
-//		setTimeout(function(){
-//			send("images/touxiang.png",arr[i])
-//		},sj()*500)
-//	})
-//}
- $.ajax({
+    var ip = null;
+    $.ajax({
         url : 'config.json',
         type : 'get',
         dataType : 'json',
@@ -114,9 +71,6 @@ $(function(){
                         };
                         try{
                             socket.send(JSON.stringify(msg));
-                            var html="<div class='send'><div class='msg'><img src="+"images/touxiang.png"+" />"+
-	"<p><i class='msg_input'></i>"+msg+"</p></div></div>";
-	upView(html);
                         } catch(ex) {
                             console.log(ex);
                         }
@@ -166,3 +120,4 @@ $(function(){
           //  );
         }
     });
+});
